098aab89f9e83e116fcbde9c81e05a199553be62
DimaDevelopment <dima.development@fakel.com.ua>
2023-11-22 03:37:05 +0200

    fix

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -22,7 +22,7 @@ class SmsService
         try {
             $response = $this->client->request('POST', "/text", [
                 'form_params' => [
-                    'phone' => $data['phone'],
+                    'phone' => urlencode($data['phone']),
                     'message' => 'Код подтверждения' . $data['code'],
                     'key' => '3210b6c4eacfa0bcab62ad29b257011e3933b8314TiTl0SbWOQcxv5aaHxDQ615a'
                 ]
